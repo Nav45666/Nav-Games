a290983b9f59424c6c61b3693b124eca73a7c29c
Abhinav <168952502+Nav45666@users.noreply.github.com>
2026-04-20 21:33:16 +0530

    Use environment variable for socket connection URL

--- frontend/app/uno/page.tsx
+++ frontend/app/uno/page.tsx
@@ -3,7 +3,9 @@
 import { useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io("http://localhost:3001");
+const socket = io(
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001",
+);
 
 type Card = {
   color?: string;
